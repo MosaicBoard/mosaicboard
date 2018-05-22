1d883678a98e31b777b5004ee622270cc84b6788
brenopolanski <breno.polanski@gmail.com>
2018-05-22 14:29:43 -0300

    Add PanelContainer component

---
+++ app/src/components/Elements/Panel/PanelContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PanelContainer extends Component {
+  render() {
+    return (
+      <div className="panel">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+PanelContainer.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default PanelContainer;
