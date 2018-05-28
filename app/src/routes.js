613b6c80e039279c33f501315d0178d69c63da6c
brenopolanski <breno.polanski@gmail.com>
2018-05-28 14:11:59 -0300

    Add banner license

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018-present, MosaicBoard.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import Login from './components/Pages/Login';
 import ResetPassword from './components/Pages/ResetPassword';
 import SignUp from './components/Pages/SignUp';
